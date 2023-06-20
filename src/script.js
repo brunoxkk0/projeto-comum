a73b55b68d3a59ee8d002ea99ddff684e6942764
Bruno Caroba <bruno_caroba@hotmail.com>
2023-06-20 16:34:12 -0300

    Adicionando o texto 3

--- src/script.js
+++ src/script.js
@@ -12,7 +12,7 @@ const title =       document.querySelector("#pageTitle");
 
 document.addEventListener("DOMContentLoaded", () => {
     loadMenu();
-    loadPage(0);
+    loadPage(2);
 });
 
 const loadMenu = () => {
